be47ef29b9719a193156c8909c6e06cadcf88d5c
ivaste <ivancich.stefano@gmail.com>
2019-10-13 12:44:11 +0200

    Trie

---
+++ Trie.py
@@ -0,0 +1,45 @@
+#Trie
+class Trie:
+
+	def __init__(self):
+		self.root = {}
+			
+
+	def insert(self, word: str) -> None:
+		trie = self.root
+		
+		for i in word:
+			# add letter if not present as a child
+			if i not in trie:
+				trie[i] = {}
+			
+			# enter childe node
+			trie = trie[i]
+		
+		#attach end-of-word indicator
+		trie["#"] = "#"
+			
+	def search(self, word: str) -> bool:
+		trie = self.root
+		
+		for i in word:
+			if i not in trie:
+				return False
+			trie = trie[i]
+		
+		if "#" in trie:
+			return True
+		
+		return False
+
+
+	def startsWith(self, prefix: str) -> bool:
+		trie = self.root
+		
+		for i in prefix:
+			if i not in trie:
+				return False
+			
+			trie = trie[i]
+		
+		return True
